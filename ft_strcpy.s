308ecc66cf1a064c65d84544adaf99c0bcd5aec3
smizuoch <shotaro.mizuochi@proton.me>
2025-06-22 07:54:19 +0900

    ft_strcpy アセンブリコードのコメントを修正しました。レジスタ名の誤りを修正し、可読性を向上させました。

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -5,9 +5,9 @@ ft_strcpy:
     mov     rax, rdi        ; ← 返り値用に元 dst を退避
     xor     rcx, rcx        ; i = 0
 .loop:
-    mov     dl,  [rsi+rcx]  ; al = src[i]
-    mov     [rdi+rcx], dl   ; dst[i] = al
+    mov     dl,  [rsi+rcx]  ; dl = src[i]
+    mov     [rdi+rcx], dl   ; dst[i] = dl
     inc     rcx             ; i++
-    test    dl, dl          ; al == 0 ?
+    test    dl, dl          ; dl == 0 ?
     jne     .loop           ; 0 でなければループ
     ret                     ; rax (元 dst) を返す
